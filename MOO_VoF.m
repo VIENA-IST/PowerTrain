dc726201c86b56d290d2411a3f9279783e33180c
Francisco F. SIlva <22647533+FranciscoFSilva@users.noreply.github.com>
2018-09-26 11:30:40 +0100

    change in MOO_VoF.m
    changed some minor aspects

--- MOO_VoF.m
+++ MOO_VoF.m
@@ -1,5 +1,15 @@
+%*************************************************************************
+% Test Problem: 'MOO_VF'
+% Description: Optimization of scalar command V/f
+%   (1)constrained
+%
+% Reference : [1] Deb K, Pratap A, Agarwal S, et al. A fast and elitist 
+%   multiobjective genetic algorithm NSGA-II[J]. Evolutionary Computation. 
+%   2002, 6(2): 182-197.
+%*************************************************************************
 
 %% Initialize variables and fminbnd options
+
 
 motor = InductionMachine;
 loadTorque = 40;
@@ -7,17 +17,7 @@ Inertia = 1.48;
 
 opt = odeset('RelTol',1e-3,'AbsTol',1e-3);
 
-%%
-
-%*************************************************************************
-% Test Problem : 'MOO_VF'
-% Description:
-%   (1)constrained
-%
-% Reference : [1] Deb K, Pratap A, Agarwal S, et al. A fast and elitist 
-%   multiobjective genetic algorithm NSGA-II[J]. Evolutionary Computation. 
-%   2002, 6(2): 182-197.
-%*************************************************************************
+%% NSGA Config
 
 
 options = nsgaopt();                    % create default options structure
@@ -40,6 +40,9 @@ options.poolsize = 3;
 
 options.objfun = @TP_MOO_VIENA;         % objective function handle
 options.plotInterval = 5;               % interval between two calls of "plotnsga". 
+
+%% NSGA Run
+
 
 result = nsga2(options,motor,Inertia,loadTorque,opt);          % begin the optimization!
 
